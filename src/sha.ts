2395c1f0b447f15d6c9f2ec98bd43aa325704698
Niels Leenheer <niels@leenheer.nl>
2020-11-20 10:19:56 +0100

    fix: use setEncoding() and read() for crypto.createHash instead of digest()

--- src/sha.ts
+++ src/sha.ts
@@ -5,11 +5,12 @@ import { d } from './debug';
 export const sha = async (filePath: string) => {
   d('hashing', filePath);
   const hash = crypto.createHash('sha256');
+  hash.setEncoding('hex');
   const fileStream = fs.createReadStream(filePath);
   fileStream.pipe(hash);
   await new Promise((resolve, reject) => {
     fileStream.on('end', () => resolve());
     fileStream.on('error', (err) => reject(err));
   });
-  return hash.digest('hex');
+  return hash.read();
 };
