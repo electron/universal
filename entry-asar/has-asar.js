f95f15cddd83c8f295ae5bc65099a878648386e5
Felix Rieseberg <fr@makenotion.com>
2023-08-31 15:04:10 -0700

    test: Add linting

--- entry-asar/has-asar.js
+++ entry-asar/has-asar.js
@@ -1,4 +1,4 @@
-const path = require('path')
+const path = require('path');
 
 if (process.arch === 'arm64') {
   setPaths('arm64');
@@ -9,15 +9,15 @@ if (process.arch === 'arm64') {
 function setPaths(platform) {
   // This should return the full path, ending in something like
   // Notion.app/Contents/Resources/app.asar
-  const appPath = app.getAppPath()
-  const asarFile = `app-${platform}.asar`
+  const appPath = app.getAppPath();
+  const asarFile = `app-${platform}.asar`;
 
   // Maybe we'll handle this in Electron one day
   if (path.basename(appPath) === 'app.asar') {
-    const platformAppPath = path.join(path.dirname(appPath), asarFile)
+    const platformAppPath = path.join(path.dirname(appPath), asarFile);
 
     // This is an undocumented API. It exists.
-    app.setAppPath(platformAppPath)
+    app.setAppPath(platformAppPath);
   }
 
   process._archPath = require.resolve(`../${asarFile}`);
--- entry-asar/no-asar.js
+++ entry-asar/no-asar.js
@@ -7,15 +7,15 @@ if (process.arch === 'arm64') {
 function setPaths(platform) {
   // This should return the full path, ending in something like
   // Notion.app/Contents/Resources/app
-  const appPath = app.getAppPath()
-  const appFolder = `app-${platform}`
+  const appPath = app.getAppPath();
+  const appFolder = `app-${platform}`;
 
   // Maybe we'll handle this in Electron one day
   if (path.basename(appPath) === 'app') {
-    const platformAppPath = path.join(path.dirname(appPath), appFolder)
+    const platformAppPath = path.join(path.dirname(appPath), appFolder);
 
     // This is an undocumented private API. It exists.
-    app.setAppPath(platformAppPath)
+    app.setAppPath(platformAppPath);
   }
 
   process._archPath = require.resolve(`../${appFolder}`);
