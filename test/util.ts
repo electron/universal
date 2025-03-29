7a0002a83361b09612c48c960aba59514314e958
Erik Moura <erikian@electronjs.org>
2025-03-29 10:51:04 -0300

    add workaround for non-`.spec.ts` file

--- test/util.ts
+++ test/util.ts
@@ -6,7 +6,8 @@ import * as path from 'path';
 import plist from 'plist';
 import * as fileUtils from '../dist/cjs/file-utils';
 import { getRawHeader } from '@electron/asar';
-import { expect } from '@jest/globals';
+
+declare const expect: typeof import('@jest/globals').expect;
 
 // We do a LOT of verifications in `verifyApp` 😅
 // exec universal binary -> verify ALL asars -> verify ALL app dirs -> verify ALL asar integrity entries
